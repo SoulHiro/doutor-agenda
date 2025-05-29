1a13d13dcbc3120a77cb8897790fd208a69a549c
SoulHiro <victormts.s1@gmail.com>
2025-05-29 09:21:27 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
 import { Button } from "@/components/ui/button";
 
-
-
 export default function Home() {
-  return (
-    <div className="font-bold">
-      <Button>Clique aqui</Button>
-
-    </div>
-  );
+  return <div className="font-bold">
+    <Button>Teste</Button>
+  </div>;
 }
