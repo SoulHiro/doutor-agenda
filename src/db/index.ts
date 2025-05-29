72b41387a3dd8aca69b525b855d6ea95795c2e53
SoulHiro <victormts.s1@gmail.com>
2025-05-29 15:07:18 -0300

    feat: add clinic form page

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,9 @@ import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/neon-http";
 
-const db = drizzle(process.env.DATABASE_URL!);
+const db = drizzle(process.env.DATABASE_URL!, {
+  schema,
+});
 
+import * as schema from "./schema"
 export default db;
